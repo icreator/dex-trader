2ae495fed355273d780f668b809a054c6ab6ff63
icreator <icreator@mail.ru>
2019-05-13 15:28:08 +0300

    init 3

--- src/main/java/org/erachain/eratrader/controller/Controller.java
+++ src/main/java/org/erachain/eratrader/controller/Controller.java
@@ -1,6 +1,5 @@
 package org.erachain.eratrader.controller;
 
-import javafx.util.Pair;
 import org.erachain.eratrader.traders.TradersManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,22 +7,18 @@ import org.slf4j.LoggerFactory;
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.security.SecureRandom;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.List;
-import java.util.Timer;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
-// 04/01 +-
 
 /**
  * main class for connection all modules
@@ -31,7 +26,7 @@ import java.util.jar.Manifest;
 public class Controller extends Observable {
 
     public static final boolean DEVELOP_USE = true;
-    public static final String APP_NAME = DEVELOP_USE ? "Erachain-dev" : "Erachain";
+    public static final String APP_NAME = DEVELOP_USE ? "EraTrader-dev" : "EraTrader";
 
     public static String version = "0.01.01";
     public static String buildTime = "2019-05-19 13:33:33 UTC";
@@ -39,30 +34,14 @@ public class Controller extends Observable {
     public static final int GENERATING_MIN_BLOCK_TIME = DEVELOP_USE ? 120 : 288; // 300 PER DAY
     public static final int GENERATING_MIN_BLOCK_TIME_MS = GENERATING_MIN_BLOCK_TIME * 1000;
 
-    public static final char DECIMAL_SEPARATOR = '.';
-    public static final char GROUPING_SEPARATOR = '`';
-
-    public static TreeMap<String, Pair<BigDecimal, String>> COMPU_RATES = new TreeMap();
-
-    // TODO ENUM would be better here
-    public static final int STATUS_NO_CONNECTIONS = 0;
-    public static final int STATUS_SYNCHRONIZING = 1;
-    public static final int STATUS_OK = 2;
     private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
     public boolean useGui = true;
-    private List<Thread> threads = new ArrayList<Thread>();
     public static long buildTimestamp;
     private static Controller instance;
     private int status;
-    private boolean dcSetWithObserver = false;
-    private boolean dynamicGUI = false;
     private TradersManager tradersManager;
-    private Timer connectTimer;
-    private Random random = new SecureRandom();
 
     private boolean isStopping = false;
-    private String info;
-
 
     public static String getVersion() {
         return version;
@@ -71,12 +50,6 @@ public class Controller extends Observable {
     public static long getBuildTimestamp() {
         if (buildTimestamp == 0) {
             Date date = new Date();
-            //// URL resource =
-            //// getClass().getResource(getClass().getSimpleName() + ".class");
-            // URL resource =
-            //// Controller.class.getResource(Controller.class.getSimpleName() +
-            //// ".class");
-            // if (resource != null && resource.getProtocol().equals("file")) {
             File f = null;
             Path p = null;
             BasicFileAttributes attr = null;
@@ -125,25 +98,6 @@ public class Controller extends Observable {
         return this.status;
     }
 
-    public void start() throws Exception {
-
-
-        // CLOSE ON UNEXPECTED SHUTDOWN
-        Runtime.getRuntime().addShutdownHook(new Thread(null, null, "ShutdownHook") {
-            @Override
-            public void run() {
-                // -999999 - not use System.exit() - if freeze exit
-                stopAll(-999999);
-                //Runtime.getRuntime().removeShutdownHook(currentThread());
-            }
-        });
-
-        // CREATE NETWORK
-        this.tradersManager = new TradersManager();
-
-    }
-
-
     public boolean isOnStopping() {
         return this.isStopping;
     }
@@ -153,9 +107,6 @@ public class Controller extends Observable {
         if (this.isStopping)
             return;
         this.isStopping = true;
-
-        if (this.connectTimer != null)
-            this.connectTimer.cancel();
 
     }
 
@@ -190,27 +141,7 @@ public class Controller extends Observable {
             LOGGER.info("Starting %app%".replace("%app%", Controller.APP_NAME));
             LOGGER.info(version + " build " + buildTime);
 
-            String licenseFile = "Erachain Licence Agreement (genesis).txt";
-            File f = new File(licenseFile);
-            if (!f.exists()) {
-
-                LOGGER.error("License file not found: " + licenseFile);
-
-                //FORCE SHUTDOWN
-                System.exit(3);
-
-            }
-
-
-            //STARTING NETWORK/BLOCKCHAIN/RPC
-
-            start();
-
-            if (!useGui) {
-                LOGGER.info("-nogui used");
-            } else {
-
-            }
+            this.tradersManager = new TradersManager();
 
 
         } catch (Exception e) {
