f6e9b76788594271a5294c4280876db789df5b31
icreator <icreator@mail.ru>
2021-02-22 19:19:33 +0300

    RaterCoinMarketCapCom Horizen

--- src/main/java/org/erachain/dextrader/Raters/RaterCoinMarketCapCom.java
+++ src/main/java/org/erachain/dextrader/Raters/RaterCoinMarketCapCom.java
@@ -3,6 +3,7 @@ package org.erachain.dextrader.Raters;
 
 import org.erachain.dextrader.settings.Settings;
 import org.erachain.dextrader.traders.TradersManager;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.slf4j.Logger;
@@ -13,7 +14,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 // see https://pro.coinmarketcap.com/account/
-/// result   "last":9577.769,"buy":9577.769,"sell":9509.466
 public class RaterCoinMarketCapCom extends Rater {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RaterCoinMarketCapCom.class);
@@ -63,76 +63,24 @@ public class RaterCoinMarketCapCom extends Rater {
         if (json == null)
             return;
 
-        //logger.info("WEX : " + result);
-
-        JSONObject pair;
         BigDecimal price;
-        BigDecimal rateLast = null;
-        BigDecimal rateBuy = null;
-        BigDecimal rateSell = null;
+        JSONObject item;
+        if (json.containsKey("data")) {
+            JSONArray array = (JSONArray) json.get("data");
+            for (Object obj: array) {
+                item = (JSONObject) obj;
+                if (item.get("name").toString().equals("Horizen")) {
+                    // HORIZEN
+                    price = new BigDecimal((double)((JSONObject)((JSONObject)item.get("quote")).get("USD")).get("price"))
+                            .setScale(16, BigDecimal.ROUND_HALF_UP);
+                    if (cnt.DEVELOP_USE) {
+                        setRate(1105L, 1108L, this.courseName, price);
+                    } else {
+                        setRate(12L, 92L, this.courseName, price);
+                    }
 
-        if (json.containsKey("btc_rur")) {
-            rateBuy = null;
-            pair = (JSONObject) json.get("btc_rur");
-            try {
-                rateLast = new BigDecimal(pair.get("last").toString());
-                rateBuy = new BigDecimal(pair.get("buy").toString());
-                rateSell = new BigDecimal(pair.get("sell").toString());
-            } catch (Exception e){
-                LOGGER.error(e.getMessage(), e);
-            }
-
-            if (rateBuy != null) {
-                price = calcPrice(rateBuy, rateSell, rateLast);
-                if (cnt.DEVELOP_USE) {
-                    setRate(1105L, 1108L, this.courseName, price);
-                } else {
-                    setRate(12L, 92L, this.courseName, price);
                 }
             }
-
         }
-
-        if (json.containsKey("btc_usd")) {
-            rateBuy = null;
-            pair = (JSONObject) json.get("btc_usd");
-            try {
-                rateLast = new BigDecimal(pair.get("last").toString());
-                rateBuy = new BigDecimal(pair.get("buy").toString());
-                rateSell = new BigDecimal(pair.get("sell").toString());
-            } catch (Exception e){
-                LOGGER.error(e.getMessage(), e);
-            }
-
-            if (rateBuy != null) {
-                price = calcPrice(rateBuy, rateSell, rateLast);
-                if (cnt.DEVELOP_USE) {
-                    setRate(1105L, 1107L, this.courseName, price);
-                } else {
-                    setRate(12L, 95L, this.courseName, price);
-                }
-            }
-
-        }
-
-        if (json.containsKey("usd_rur")) {
-            rateBuy = null;
-            pair = (JSONObject) json.get("usd_rur");
-            try {
-                rateLast = new BigDecimal(pair.get("last").toString());
-                rateBuy = new BigDecimal(pair.get("buy").toString());
-                rateSell = new BigDecimal(pair.get("sell").toString());
-            } catch (Exception e){
-                    LOGGER.error(e.getMessage(), e);
-            }
-
-            if (rateBuy != null) {
-                price = calcPrice(rateBuy, rateSell, rateLast);
-                setRate(1077L, 1078L, this.courseName, price);
-                LOGGER.info("WEX rate: USD - RUR " + price);
-            }
-
-        }
-
     }
 }
